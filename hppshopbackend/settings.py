4e44b43c1e0b135f1a0c5fead64fcc0ffc099bb3
keroo <kerooadel5@gmail.com>
2023-12-07 20:32:23 +0200

    edit setting db

--- hppshopbackend/settings.py
+++ hppshopbackend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -106,22 +107,32 @@ WSGI_APPLICATION = 'hppshopbackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+# DATABASES = {
+#     # 'default': {
+#     #     'ENGINE': 'django.db.backends.sqlite3',
+#     #     'NAME': BASE_DIR / 'db.sqlite3',
+#     # }
     
-       'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': "hpp_shop",
-       'USER': 'postgres',
-       'PASSWORD': '123456',
-       'HOST': 'localhost',
-       'PORT': '5432',
-   }
-}
+#        'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': "hpp_shop",
+#        'USER': 'postgres',
+#        'PASSWORD': '123456',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+# }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DATABASE', 'default_db_name'),
+        'USER': os.environ.get('POSTGRES_USER', 'default_db_user'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'default_db_password'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),  # Adjust the variable name if needed
+    }
+}
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
